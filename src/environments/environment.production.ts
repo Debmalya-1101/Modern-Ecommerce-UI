104bcf03657078c350909c6a48034d60ff7d9f27
Debmalya Dutta <160530222+Debmalya-1101@users.noreply.github.com>
2026-06-24 18:38:42 +0530

    configure environments and vercel for deployment

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -2,7 +2,7 @@ import { AppEnvironment } from './environment.model';
 
 export const environment: AppEnvironment = {
   production: true,
-  apiBaseUrl: 'https://api.modern-commerce.example',
+  apiBaseUrl: 'https://my-app-nexis-be.onrender.com',
   tokenStorageKey: 'modern-commerce-token',
   razorpayKeyId: 'rzp_test_T1vzqSilDkrVwC'
 };
